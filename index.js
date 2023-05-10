9f3bf731c4ad7778c4d7414e3fe16bfbbcb57d0d
JayeshYadav57 <jayeshyadav8080@gmail.com>
2023-05-10 16:03:50 +0530

    Updated color and background color

--- index.js
+++ index.js
@@ -1,65 +1,57 @@
-let i=0;
-let txt="Full Stack Developer.";
-let speed=85;
+let i = 0;
+let txt = "Full Stack Developer.";
+let speed = 85;
 
-typeWriter()
+typeWriter();
 
 function typeWriter() {
-    if(i < txt.length){
-      document.getElementById("user-detail-name").style.color="white";
-      document.getElementById("demo1").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
+    if (i < txt.length) {
+        document.getElementById("demo1").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else {
+        // reset the index when it reaches the end of the text
+        i = 0;
+        // clear the text
+        document.getElementById("demo1").innerHTML = "";
+        setTimeout(typeWriter, speed);
     }
-    else {
-      // reset the index when it reaches the end of the text
-      i = 0;
-      // clear the text
-      document.getElementById("demo1").innerHTML = "";
-      // make the recursive call
-      document.getElementById("user-detail-name").style.color="#f51cb3";
-      setTimeout(typeWriter, speed);
+}
+
+var myNav = document.getElementById("nav-menu");
+var myNav2 = document.getElementById("main");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop >= 15) {
+        myNav.classList.add("nav-colored");
+        myNav2.classList.add("nav-colored");
+        // myNav.classList.remove("nav-transparent");
+    } else {
+        // myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+        myNav2.classList.remove("nav-colored");
     }
-  }
+};
 
-  
-  var myNav = document.getElementById('nav-menu');
-  var myNav2 = document.getElementById('main');
-  window.onscroll = function () { 
-      if ( document.documentElement.scrollTop >= 15 ) {
-          myNav.classList.add("nav-colored");
-          myNav2.classList.add("nav-colored");
-          // myNav.classList.remove("nav-transparent");
-      } 
-      else {
-          // myNav.classList.add("nav-transparent");
-          myNav.classList.remove("nav-colored");
-          myNav2.classList.remove("nav-colored");
-      }
-  };
-
-  GitHubCalendar(".calendar", "JayeshYadav07", {
+GitHubCalendar(".calendar", "JayeshYadav07", {
     responsive: true,
     global_stats: false,
     tooltips: true,
-  });
+});
 
-  document.getElementById("resume-button-1").onclick=()=>{
-window.open("./images/Jayesh_Yadav_Resume.pdf");
-  }
-
-  document.getElementById("resume-button-2").onclick=()=>{
+document.getElementById("resume-button-1").onclick = () => {
     window.open("./images/Jayesh_Yadav_Resume.pdf");
-  }
-     
-  
-  function openNav() {
+};
+
+document.getElementById("resume-button-2").onclick = () => {
+    window.open("./images/Jayesh_Yadav_Resume.pdf");
+};
+
+function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.display = "none";
-  }
-  
-  function closeNav() {
+}
+
+function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.display= "block";
-  }
-  
+    document.getElementById("main").style.display = "block";
+}
